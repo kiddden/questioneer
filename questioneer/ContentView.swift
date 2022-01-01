c69d23522418f2c15d6fdda3029b8dc7f7672308
Kazzer <kazzer.nd@gmail.com>
2022-01-02 01:27:06 +0200

    question page updated
    simplified the way the buttons work, applied DRY principle to my code and cleaned it pretty much

--- questioneer/ContentView.swift
+++ questioneer/ContentView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    var question = Question(
+        questionText: "What was the first computer bug?",
+        possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
+        correctAnswerIndex: 2)
+    
     let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
-    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
     
     var body: some View {
         ZStack{
@@ -19,52 +24,19 @@ struct ContentView: View {
                     .font(.callout)
                     .multilineTextAlignment(.leading)
                     .padding()
-                Text("Question 1?")
+                Text(question.questionText)
                     .font(.largeTitle)
                     .bold()
                     .multilineTextAlignment(.leading)
                 Spacer()
                 HStack{
-                    Button(action: {
-                        print("Tapped the 1 button")
-                    }, label: {
-                        Text("Button")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
-                    Button(action: {
-                        print("Tapped the 2 button")
-                    }, label: {
-                        Text("Button")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
-                    Button(action: {
-                        print("Tapped the 3 button")
-                    }, label: {
-                        Text("Button")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
-                    Button(action: {
-                        print("Tapped the 4 button")
-                    }, label: {
-                        Text("Button")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
+                    ForEach(0..<question.possibleAnswers.count) { answerIndex in
+                        Button(action: {
+                            print("Tapped on option with the text : \(question.possibleAnswers[answerIndex])")
+                          }, label: {
+                            ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
+                          })
+                    }
                 }
             }
         }
---
+++ questioneer/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  questioneer
+//
+//  Created by Eugene Ned on 02.01.2022.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
