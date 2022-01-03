a146b8bbe6e9e4aa9e9268939700e8036d9c4101
Kazzer <kazzer.nd@gmail.com>
2022-01-03 23:57:47 +0200

    even more refactoring
    question view updated, from this moment button changes its color when depending on your answer + you can't choose an answer again after it shows the answer.
    The 'back' button is hidden from now on and you can't go to the welcome page from the active game.
    game view model developed with a couple of new computer vars and functions that helps us to change the buttons' color and block choosing after showing results

--- questioneer/Views/QuestionView.swift
+++ questioneer/Views/QuestionView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct QuestionView: View {
+    @EnvironmentObject var viewModel: GameViewModel
     let question: Question
     
     var body: some View {
@@ -21,9 +22,19 @@ struct QuestionView: View {
                 ForEach(0..<question.possibleAnswers.count) { answerIndex in
                     Button(action: {
                         print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
+                        viewModel.makeGuess(atIndex: answerIndex)
                     }) {
                         ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
+                            .background(viewModel.color(forOptionIndex: answerIndex))
                     }
+                    .disabled(viewModel.guessWasMade)
+                }
+            }
+            if viewModel.guessWasMade {
+                Button(action: {
+                    viewModel.displayNextScreen()
+                }) {
+                    BottomTextView(str: "Next")
                 }
             }
         }
